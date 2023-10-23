5acaa6f6f07cc7df1de46163b55bcef06f8f04ca
Sqin0608 <146144937+Sqin0608@users.noreply.github.com>
2023-10-23 21:40:29 +0000

    almost (w/o fs and rand)

--- app.rb
+++ app.rb
@@ -16,9 +16,43 @@ get '/square/results' do
   erb(:square_results)
 end
  
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+get("/square_root/new") do
+  erb(:new_root_calc)
+end
+
+get '/square_root/results' do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = @the_num ** 0.5
+  erb(:square_root_results)
+end
+
+get("/payment/new") do
+  erb(:new_payment_calc)
+end
+
+get '/payment/results' do
+  @the_apr = params.fetch("users_apr").to_f.to_fs(:percentage)
+
+  @the_year = params.fetch("users_year").to_f
+
+  @the_principal = params.fetch("users_principal").to_f.to_fs(:currency)
+
+  @the_payment = (@the_apr * @the_principal)/(1 - (1 + @the_apr) ** -@the_year).to_f.to_fs(:currency)
+
+  erb(:payment_results)
+end
+
+get("/random/new") do
+  erb(:new_random_calc)
+end
+
+get '/random/results' do
+  @the_min = params.fetch("users_max").to_f
+
+  @the_max = params.fetch("users_min").to_f
+
+  @the_random = rand(@the_min..@the_max).to_f
+
+  erb(:random_results)
 end
